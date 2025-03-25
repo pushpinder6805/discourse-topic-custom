ff8fbd0e4b752979ce524f070089a5edaef49fdc
pinder99 <pinder.chaudhary@gmail.com>
2025-03-25 18:04:23 +0530

    Create remove-all-columns.js

---
+++ javascripts/discourse/api-initializers/remove-all-columns.js
@@ -0,0 +1,15 @@
+import { apiInitializer } from "discourse/lib/api";
+
+export default apiInitializer("1.34", (api) => {
+  api.registerValueTransformer("topic-list-columns", ({ value: columns }) => {
+    // Remove all core columns
+    columns.delete("posters");   // Avatar images
+    columns.delete("topic");     // Topic title + category
+    columns.delete("likes");     // Like count
+    columns.delete("replies");   // Number of replies
+    columns.delete("views");     // Number of views
+    columns.delete("activity");  // Last activity timestamp
+
+    return columns;
+  });
+});
